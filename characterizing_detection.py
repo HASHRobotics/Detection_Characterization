c9dbd9840d2d787e71738f80cd97e3c99f7276b0
Harsh Sharma <harsh@Harshs-MacBook-Pro.local>
2019-04-19 20:25:39 -0400

    Pipeline for detection characterisation completed

--- characterizing_detection.py
+++ characterizing_detection.py
@@ -1,3 +1,5 @@
+
+
 import numpy as np
 import cv2
 import ipdb 
@@ -6,10 +8,13 @@ import glob
 import csv
 import matplotlib.pyplot as plt
 import sys
+import detection_v2 as dt
+import imutils
+
 
 '''
 INPUT: Image of the rover with marked centroid (by algo). 
-	   Range at which that image was captured (set a default value incase range is not received)
+       Range at which that image was captured (set a default value incase range is not received)
 DISPLAY: Intermediate results for the video (to be shown in demonstration)
 Output: Percentage of correct detection
 
@@ -19,43 +24,62 @@ Set a threshold of euclidian distance between these two rovers (as a function of
 incorrectly detected else as correctly detected. 
 '''
 
-def point_click(filepath):
+def point_click(img):
 
-	img = cv2.imread(filepath,-1)
-	_, ax = plt.subplots(figsize=(9, 9))
-	img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-	imgplot = ax.imshow(img)
-	x_c, y_c = plt.ginput(1, mouse_stop=2)[0]		#y_c is the row while x_c is the column
-	print(y_c,x_c)
-	cv2.destroyAllWindows()
+    _, ax = plt.subplots(figsize=(9, 9))
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    imgplot = ax.imshow(img)
+    x_c, y_c = plt.ginput(1, mouse_stop=2)[0]       #y_c is the row while x_c is the column
+    print(y_c,x_c)
+    cv2.destroyAllWindows()
 
-	return y_c,x_c
+    return y_c,x_c
 
 def find_detection_rate(error_array,distance_metric):
-	correct_detections = len(error_array[np.where( error_array < distance_metric)])
-	return correct_detections/error_array.shape[0]
+    print(error_array)
+    correct_detections = len(error_array[np.where( error_array < distance_metric)])
+    return correct_detections/error_array.shape[0]
 
 if __name__ == "__main__":
 
-	images_path = sys.argv[1]
-	images_path = '../image_data/'	#Remove this
-	centroid_using_algo_csv = sys.argv[2]
-	centroid_using_algorithm = np.genfromtxt(centroid_using_algo_csv, delimiter=',')
-	distance_metric = 20 #(in pixels)
+    images_path = sys.argv[1]
+    images_path = '../image_data/'  #Remove this
+    #centroid_using_algo_csv = sys.argv[2]
+    #centroid_using_algorithm = np.genfromtxt(centroid_using_algo_csv, delimiter=',')
+    distance_metric = 80 #(in pixels)
+    flag = 0
+    items = os.listdir(images_path)
+    if('.DS_Store' in items):
+        items.remove('.DS_Store')
 
 
-	for i,filename in enumerate(os.listdir(images_path)):
+    for i,filename in enumerate(items):
 
-		y_pc,x_pc = point_click(images_path + filename)
+        img = cv2.imread(images_path + filename,-1)
+        new_height = int(600)
+        new_width = int(900)
 
-		if(i==0):
-			centroid_using_point_click = np.array([y_pc,x_pc])
-		else:
-			centroid_using_point_click = np.vstack((centroid_using_point_click,np.array([y_pc,x_pc])))
+        img = imutils.resize(img, height=new_height, width=new_width)
 
-	error_array = np.linalg.norm(np.abs(centroid_using_point_click - centroid_using_algorithm),axis = 1)
-	detection_rate = find_detection_rate(error_array,distance_metric)
-	print("Detection rate is ",detection_rate*100," % \n")
+        cX,cY = dt.get_centroid(img)
+        y_pc,x_pc = point_click(img)
+        print("Centroid detected by algo is: ",cY,"\t",cX,"\n")
+        print("Centroid detected by point and click is: ",y_pc,"\t",x_pc,"\n")
+        print("\n ===================================================================================================== \n")
+
+        if(flag==0 and cX!=-1):
+            flag+=1
+            centroid_using_algorithm = np.array([cY,cX])
+            centroid_using_point_click = np.array([y_pc,x_pc])
+        elif(cX!=-1):
+            centroid_using_algorithm = np.vstack((centroid_using_algorithm,np.array([cY,cX])))
+            centroid_using_point_click = np.vstack((centroid_using_point_click,np.array([y_pc,x_pc])))
+
+    assert  centroid_using_algorithm.shape == centroid_using_point_click.shape, "Shapes of the two arrays don't match"
+    error_array = np.linalg.norm(np.abs(centroid_using_point_click - centroid_using_algorithm),axis = 1)
+    detection_rate = find_detection_rate(error_array,distance_metric)
+    print("Detection rate is ",detection_rate*100," % \n")
+
 
 
 
